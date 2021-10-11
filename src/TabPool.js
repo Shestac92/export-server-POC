120a9ed17b287046543e89641eaa5811ae27ecb2
Alexander Shestakov <shestacov92@gmail.com>
2021-10-11 21:42:22 +0800

    merge sizable export as the most actual implementation

--- src/TabPool.js
+++ src/TabPool.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-await-in-loop */
-const { chromium } = require('playwright-chromium');
-const { join } = require('path');
+import { chromium } from 'playwright-chromium';
+import { join } from 'path';
 
-module.exports = class TabPool {
+export default class TabPool {
   constructor(screenshotsDirPath, tabsCount = 5) {
     this.tabsCount = tabsCount;
     this.screenshotsDirPath = screenshotsDirPath;
     this.isHeadless = true;
-    this.pages = [];
+    this.workers = [];
     this.browser = null;
     this.context = null;
   }
@@ -15,32 +15,48 @@ module.exports = class TabPool {
   async initiateBrowser() {
     if (this.browser) return;
 
-    this.browser = await chromium.launch({ headless: this.isHeadless });
+    this.browser = await chromium.launch(
+      { headless: this.isHeadless, viewport: { width: 1280, height: 1280 } }
+    );
     this.context = await this.browser.newContext({ ignoreHTTPSErrors: true, locale: 'en-US' });
   }
 
   async populatePages() {
-    if (this.pages.length) return;
+    if (this.workers.length) return;
 
     for (let i = 0; i < this.tabsCount; i++) {
       const page = await this.context.newPage();
-      this.pages.push({ page, isIdle: true });
+      this.workers.push({ page, isIdle: true });
     }
   }
 
   getIdlePage() {
     for (let i = 0; i < this.tabsCount; i++) {
-      if (this.pages[i].isIdle) return i;
+      if (this.workers[i].isIdle) return i;
     }
     return -1;
   }
 
-  async assignExportTask(pageIndex, html) {
-    const page = this.pages[pageIndex];
-    page.isIdle = false;
-    await page.page.setContent(html);
+  async assignExportTask(pageIndex, html, width, height) {
+    const worker = this.workers[pageIndex];
+    const { page } = worker;
+    worker.isIdle = false;
+    // TODO: найти решение с отрисовкой
+    // await page.page.route('**', route => route.continue());
+    // await page.page.reload({ waitUntil: 'domcontentloaded' });
+    await page.goto('http://localhost:3000', { waitUntil: 'domcontentloaded' });
+    await page.setContent(html, { waitUntil: 'domcontentloaded' });
     const path = join(this.screenshotsDirPath, `${process.hrtime.bigint()}.jpg`);
-    await page.page.screenshot({ path });
-    page.isIdle = true;
+    // TODO: quality, jpg/png
+    await page.screenshot({
+      path,
+      clip: {
+        x: 0,
+        y: 0,
+        width,
+        height
+      }
+    });
+    worker.isIdle = true;
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const monitor = require('express-status-monitor');
-const { mkdirSync, existsSync } = require('fs');
-const { resolve } = require('path');
-const TabPool = require('./TabPool');
+import express from 'express';
+import monitor from 'express-status-monitor';
+import { mkdirSync, existsSync } from 'fs';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import TabPool from './TabPool';
 
 const PORT = 3000;
-const screenshotsDirPath = resolve(__dirname, '..', 'screenshots');
+const screenshotsDirPath = resolve(dirname(fileURLToPath(import.meta.url)), '..', 'screenshots');
 const app = express();
-const jsonParser = bodyParser.json();
 const tabPool = new TabPool(screenshotsDirPath, 5);
 
-const getHtmlContent = json => `
+const getHtmlContent = (json, width, height) => `
   <html>
       <head>
           <title>Example</title>
           <style>html, body, #container {
-            width: 100%;
-            height: 100%;
+            width: ${width}px;
+            height: ${height}px;
             margin: 0;
             padding: 0;
         }</style>
       </head>
       <body>
         <div id="container"></div>
-        <script src="https://cdn.anychart.com/releases/8.10.0/js/anychart-bundle.min.js"></script>
-        <script type="text/javascript">anychart.onDocumentReady(function () {
+        <script src="http://localhost:3000/anychart-bundle.min.js"></script>
+        <script type="text/javascript">
           var json = ${json};
           var chart = anychart.fromJson(json);
-          chart.container('container').draw();
-        });</script>
+          chart.animation(false);
+          chart.container('container').draw();</script>
       </body>
   </html>`;
 
@@ -43,9 +42,10 @@ const handleQueue = async () => {
   if (pageIndex === -1) return;
 
   const { req, res, start } = queue.shift();
-  const json = JSON.stringify(req.body);
-  const html = getHtmlContent(json);
-  await tabPool.assignExportTask(pageIndex, html);
+  const { config, width = 600, height = 600 } = req.body;
+  const json = JSON.stringify(config);
+  const html = getHtmlContent(json, width, height);
+  await tabPool.assignExportTask(pageIndex, html, width, height);
   res.end();
   const end = process.hrtime.bigint();
   console.log(`Handler execution time ${Number(end - start) / 1000000} ms`);
@@ -59,8 +59,9 @@ if (!existsSync(screenshotsDirPath)) {
 
 app.use(express.static('public'));
 app.use(monitor());
+app.use(express.json());
 
-app.post('/', jsonParser, async (req, res) => {
+app.post('/', async (req, res) => {
   const start = process.hrtime.bigint();
 
   queue.push({ req, res, start });
@@ -71,11 +72,6 @@ app.get('/stop', async (req, res) => {
   await browser.close();
   res.send();
   process.exit(0);
-});
-
-// dummy route for loading tests
-app.get('/dummy', (req, res) => {
-  res.send();
 });
 
 app.listen(PORT, async () => {
